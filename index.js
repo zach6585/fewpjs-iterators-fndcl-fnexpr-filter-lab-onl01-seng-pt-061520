e06ca361d9d9a9f0f4df9632b87cd12c175d99e7
zach6585 <zmines@wesleyan.edu>
2021-01-11 20:10:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, passedName) {
+   return drivers.filter(n => {
+       return n.toUpperCase() == passedName.toUpperCase();
+   }) 
+}
+
+function fuzzyMatch(drivers, passedName) {
+    return drivers.filter(n => {
+        return n[0].toUpperCase() == passedName[0].toUpperCase();
+    })
+}
+
+function matchName(drivers, passedName) {
+    return drivers.filter(n => {
+        return n["name"] == passedName;
+    })
+}
